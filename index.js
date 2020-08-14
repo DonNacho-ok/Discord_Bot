fb3019c48dd1a80ae0fa484804415c9e9bd71334
DonNacho-ok <56751437+DonNacho-ok@users.noreply.github.com>
2020-08-14 02:22:17 -0300

    mejora de codigo y añadido de comandos 2

--- index.js
+++ index.js
@@ -7,16 +7,39 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-  if(message.content == '+ping'){
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if(command === 'ping'){
     message.channel.send('PONG, LA CONCHA DE TU MADRE!')
   }
 
-  if(message.content == `${prefix}angularvsreact`){
+  if(command === `angularvsreact`){
     message.channel.send("No me preguntes a mi, solo soy un script de node...")
   }
 
-  if(message.content == `${prefix}usercount`){
+  if(command === `usercount`){
     message.channel.send(`La cantidad de usuarios en este servidor es de ${message.guild.memberCount} usuarios`)
+  }
+
+  if (command === 'args-info') {
+    if (!args.length) {
+      return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+	  }
+
+    message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+  }
+
+  if(command === "kick"){
+    if(!message.mentions.users.size){
+      return message.reply("tenes que taggear a alguien, aweonao")
+    }
+    
+    const taggedUser = message.mentions.users.first();
+
+    message.channel.send(`Trataste de kickear a ${taggedUser.username}, puto`)
   }
 });
 
